10ecb7463529d0bed76897a6e2b647a9232f2962
Alexandr Karensky <skittles8bit@gmail.com>
2021-06-13 13:04:23 +0300

    init: implementation Stock Maximize func

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,33 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        if prices.count < 2 {
+            return 0
+        }
+        
+        var profit = 0
+        guard var maxValueOfPrices = prices.max() else { return 0 }
+        
+        for (index, elem ) in prices.enumerated() {
+            if index < prices.endIndex - 1 {
+                guard let firstIndex = prices.firstIndex(of: maxValueOfPrices) else { return 0 }
+            
+                if ( elem < maxValueOfPrices && index < firstIndex) {
+                    profit += maxValueOfPrices - elem
+                }
+            
+                if (elem < maxValueOfPrices && index > firstIndex) {
+                    
+                    guard let temp = prices[(firstIndex + 1)...].max() else { return 0 }
+                    maxValueOfPrices = temp
+                    
+                    if (elem < maxValueOfPrices) {
+                        profit += maxValueOfPrices - elem
+                    }
+                }
+            }
+        }
+
+        return profit
     }
 }
